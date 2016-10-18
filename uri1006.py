31e16274ce082675c3eeb7ea95fdabe486d4fe6e
Yúri Faro <yurifarod@yahoo.com.br>
2016-10-18 17:30:40 -0200

    Create uri1006.py

---
+++ uri1006.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+a = float(raw_input())
+b = float(raw_input())
+c = float(raw_input())
+a = round(a,1)
+b = round(b,1)
+c = round(c,1)
+media = a*0.2+b*0.3+c*0.5
+print "MEDIA = %.1f" %media
