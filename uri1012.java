51539a4b915fa7516f464f70f3e18358b30f7b3d
Yúri Faro <yurifarod@yahoo.com.br>
2016-10-18 17:27:37 -0200

    Update uri1012.java

--- uri1012.java
+++ uri1012.java
@@ -3,18 +3,29 @@ import java.util.Scanner;
 
 public class Main {
 
-public static void main(String[] args) {
+	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
 		DecimalFormat df = new DecimalFormat("0.000");
 		
-		int a = Integer.parseInt(input.nextLine());
-		int b = Integer.parseInt(input.nextLine());
+		Double a = input.nextDouble();
+		Double b = input.nextDouble();
+		Double c = input.nextDouble();
 		
-		Double litros = ((double)(a)*(double)b)/12.0;
-		
-		System.out.println(df.format(litros));
 		input.close();
+		Double triangulo = a*c/2;
+		Double circulo = 3.14159*c*c;
+		Double quadrado = b*b;
+		Double retangulo = a*b;
+		Double trapezio = (b+a)*c/2;
+		
+		System.out.println("TRIANGULO: "+df.format(triangulo));
+		System.out.println("CIRCULO: "+df.format(circulo));
+		System.out.println("TRAPEZIO: "+df.format(trapezio));
+		System.out.println("QUADRADO: "+df.format(quadrado));
+		System.out.println("RETANGULO: "+df.format(retangulo));
+
+
 	}
 
 }
