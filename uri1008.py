41d8fb0eac673d520e6fe3c2d498f21d7fea48fd
Yúri Faro <yurifarod@yahoo.com.br>
2016-10-18 17:28:43 -0200

    Create uri1008.py

---
+++ uri1008.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+num = int(raw_input())
+horas = int(raw_input())
+valor = float(raw_input())
+
+salario = horas*valor
+
+print "NUMBER = %i" %num
+print "SALARY = U$ %.2f" %salario
