6b5004381aed1f1fc14ae6acfe749f8840b43a8d
Yúri Faro <yurifarod@yahoo.com.br>
2018-01-16 16:53:39 -0200

    Add files via upload

--- uri1161.cpp
+++ uri1161.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 int main(){
 
-  unsigned long a[20], t;
+  unsigned long long a[21], t;
   int n1, n2;
 
   a[0] = 1;
 
   for(int i = 1; i < 21; i++){
-    a[i] = a[i-1] * i;
+    a[i] = a[i-1] * i *1LL;
   }
 
   while(scanf("%d %d", &n1, &n2)!= EOF){
     t = a[n1]+a[n2];
-    printf("%lu\n", t);
+    printf("%llu\n", t);
   }
-  
+
   return 0;
 }
