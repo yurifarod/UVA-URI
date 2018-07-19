3c13b225d6858fb3c6bbdd218a09a215784f49d3
Yúri Faro <yurifarod@yahoo.com.br>
2018-07-19 10:11:55 -0300

    Add files via upload

---
+++ uri1041.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+using namespace std;
+int main(){
+  float x, y;
+  scanf("%f %f", &x, &y);
+  if((x == 0) && (y == 0)){
+    printf("Origem\n");
+  }
+  if((x != 0) && (y == 0)){
+    printf("Eixo X\n");
+  }
+  if((x == 0) && (y != 0)){
+    printf("Eixo Y\n");
+  }
+  if((x > 0) && (y > 0)){
+    printf("Q1\n");
+  }
+  if((x < 0) && (y > 0)){
+    printf("Q2\n");
+  }
+  if((x < 0) && (y < 0)){
+    printf("Q3\n");
+  }
+  if((x > 0) && (y < 0)){
+    printf("Q4\n");
+  }
+  return 0;
+}
